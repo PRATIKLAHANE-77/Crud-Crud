a75f236e2455e3148bf310da64643ee268056489
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-08-15 13:07:43 +0530

    after refreshing the page my information restored

--- main.js
+++ main.js
@@ -1,73 +1,44 @@
-document
-  .getElementById("submit-btn")
-  .addEventListener("click", function display(event) {
-    event.preventDefault();
-    const mainlist = document.getElementById("list");
-    const amount = document.getElementById("amount").value;
-    const category = document.getElementById("category").value;
-    const description = document.getElementById("description").value;
-    let obj = {
-      amount: amount,
-      category: category,
-      description: description,
-    };
-    let uid =
-      JSON.stringify(amount) +
-      JSON.stringify(category) +
-      JSON.stringify(description);
+const submitbtn = document.getElementById("submit-btn"); // grab the submit button
 
-      axios.post("https://crudcrud.com/api/48854ad230894cc6b330754f75016182/Info", obj).then((Response)=>{
-        console.log(Response)
-      }).catch((err)=>{
-        console.log(err);
-      })
-    localStorage.setItem(uid, JSON.stringify(obj));
-    const newli = document.createElement("li");
-    newli.id = "newli";
-    newli.textContent = localStorage.getItem(uid);
-    const deletebtn = document.createElement("button");
-    deletebtn.id = "delete-btn";
-    deletebtn.className = "btn btn-danger btn-lg";
-    deletebtn.textContent = "Delete";
-    newli.appendChild(deletebtn);
-    const Editbtn = document.createElement("button");
-    Editbtn.className = "btn btn-warning btn-lg";
-    Editbtn.textContent = "Edit";
-    newli.appendChild(Editbtn);
-    mainlist.appendChild(newli);
+submitbtn.addEventListener("click", process); // added eventlisterner to submit button
 
-    // delete functionality
-    deletebtn.addEventListener("click", function deleteInfo(event) {
-      event.preventDefault();
-      localStorage.removeItem(uid); /// how it work
-      mainlist.removeChild(deletebtn.parentElement);
+function process(event) {
+  event.preventDefault();
+
+  const amount = document.getElementById("amount").value; // fetching the values which we typed
+  const category = document.getElementById("category").value;
+  const description = document.getElementById("description").value;
+
+  let obj = {
+    amount: amount,
+    category: category,
+    description: description,
+  };
+
+  postdata(obj); // giving this values to the backend
+}
+
+function postdata(param) {
+  axios
+    .post(
+      "https://crudcrud.com/api/8b9e717c996b41d3a24a02407761481d/Info",param)
+    .then((response) => {
+      display(response.data); // calling getdata for getting the data which we posted
+    })
+    .catch((err) => {
+      display(err);
     });
-    Editbtn.addEventListener("click", function editInfo(event) {
-      event.preventDefault();
-      mainlist.removeChild(newli);
-      document.getElementById("amount").value = obj.amount;
-      document.getElementById("category").value = obj.category;
-      document.getElementById("description").value = obj.description;
-    });
-  });
+}
 
+window.addEventListener('DOMContentLoaded', () => {
+  axios.get("https://crudcrud.com/api/8b9e717c996b41d3a24a02407761481d/Info").then((response)=>{
+    display(response.data)
+  }).catch((err)=>{
+    display(err);
+  })
+})
 
-// ask in doubt session
-// const Name = (arr)=>{
-// let count = 0;
-
-//    return () => {
-//     console.log("hello" + " " + arr[count]);
-//     count++;
-   
-// }
-
-// }
-
-// let fun = Name(["Ram","Shyam"]);
-// // console.log(fun);
-
-// fun()// Print Hello Ram
-
-// fun()//print Hello Shyam
-
+function display(param2) {
+  document.getElementById("div-id").innerHTML =
+    document.getElementById("div-id").innerHTML + JSON.stringify(param2); // displaying the data
+}
